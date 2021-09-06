b514e51b79b498823e910fc07057bdfda38c3d7f
Athos F. Bernardo <80980718+AthosFB@users.noreply.github.com>
2021-09-06 10:51:00 -0300

    initial files

---
+++ js/script.js
@@ -0,0 +1,180 @@
+let m1 = window.document.getElementById("md1")
+let m2 = window.document.getElementById("md2")
+let m3 = window.document.getElementById("md3")
+let busc = window.document.getElementById("busca")
+let vrm1 = window.document.getElementById("vrm1")
+let vrm2 = window.document.getElementById("vrm2")
+let vrm3 = window.document.getElementById("vrm3")
+let img1 = window.document.getElementById("mdimg1")
+let img2 = window.document.getElementById("mdimg2")
+let img3 = window.document.getElementById("mdimg3")
+function ent1() {
+    m1.style.width = "620px"
+    m1.style.height = "442px"
+
+    m2.style.width = "320px"
+    m2.style.height = "229px"
+
+    m3.style.width = "320px"
+    m3.style.height = "229px"
+
+    vrm1.innerText = ""
+    vrm1.style.width = "0px"
+    vrm1.style.height = "0px"
+
+    vrm2.style.marginTop = "0px"
+    vrm2.style.marginLeft = "50px"
+
+    vrm3.style.marginTop = "0px"
+    vrm3.style.marginLeft = "50px"
+
+    img1.style.marginLeft = "200px"
+    img2.style.marginTop = "0px"
+    img3.style.marginTop = "0px"
+
+    img1.style.boxShadow = "0px 0px 0px 6px rgb(214, 61, 228)"
+    img2.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+    img3.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+
+    img2.style.marginLeft = "55px"
+    img3.style.marginLeft = "55px"
+}
+function ent2() {
+    m2.style.width = "620px"
+    m2.style.height = "442px"
+
+    m1.style.width = "320px"
+    m1.style.height = "229px"
+
+    m3.style.width = "320px"
+    m3.style.height = "229px"
+
+    vrm2.innerText = ""
+    vrm2.style.width = "0px"
+    vrm2.style.height = "0px"
+
+    vrm1.style.marginTop = "10px"
+    vrm1.style.marginLeft = "50px"
+
+    vrm3.style.marginTop = "10px"
+    vrm3.style.marginLeft = "50px"
+
+    img1.style.marginTop = "0px"
+    img2.style.marginLeft = "200px"
+    img3.style.marginTop = "0px"
+
+    img2.style.boxShadow = "0px 0px 0px 6px rgb(214, 61, 228)"
+    img1.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+    img3.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+
+    img1.style.marginLeft = "55px"
+    img3.style.marginLeft = "55px"
+}
+function ent3() {
+    m3.style.width = "620px"
+    m3.style.height = "442px"
+
+    m2.style.width = "320px"
+    m2.style.height = "229px"
+    m2.style.marginTop = "100px"
+
+    m1.style.width = "320px"
+    m1.style.height = "229px"
+
+    vrm3.innerText = ""
+    vrm3.style.width = "0px"
+    vrm3.style.height = "0px"
+
+    vrm1.style.marginTop = "10px"
+    vrm1.style.marginLeft = "50px"
+
+    vrm2.style.marginTop = "10px"
+    vrm2.style.marginLeft = "50px"
+
+    img1.style.marginTop = "0px"
+    img2.style.marginTop = "0px"
+    img3.style.marginLeft = "200px"
+
+    img3.style.boxShadow = "0px 0px 0px 6px rgb(214, 61, 228)"
+    img1.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+    img2.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+
+    img1.style.marginLeft = "55px"
+    img2.style.marginLeft = "55px"
+}
+function entbusc() {
+    busc.style.width = "540px"
+    busc.style.height = "1500px"
+
+    m1.style.width = "340px"
+    m1.style.height = "200px"
+
+    m2.style.width = "340px"
+    m2.style.height = "200px"
+
+    m3.style.width = "340px"
+    m3.style.height = "200px"
+
+    vrm1.style.marginLeft = "60px"
+    vrm2.style.marginLeft = "60px"
+    vrm3.style.marginLeft = "60px"
+
+    vrm1.style.marginTop = "0px"
+    vrm2.style.marginTop = "0px"
+    vrm3.style.marginTop = "0px"
+
+    img1.style.marginTop = "0px"
+    img2.style.marginTop = "0px"
+    img3.style.marginTop = "0px"
+
+    img1.style.marginLeft = "60px"
+    img2.style.marginLeft = "60px"
+    img3.style.marginLeft = "60px"
+
+    img1.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+    img2.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+    img3.style.boxShadow = "0px 0px 0px 0px rgb(214, 61, 228)"
+}
+function saiu() {
+    m1.style.width = "420px"
+    m1.style.height = "300px"
+
+    m2.style.width = "420px"
+    m2.style.height = "300px"
+    m2.style.marginTop = "82px"
+
+    m3.style.width = "420px"
+    m3.style.height = "300px"
+
+    busc.style.width = "300px"
+    busc.style.height = "1500px"
+
+    vrm1.innerText = "Ver Mais"
+    vrm1.style.width = "210px"
+    vrm1.style.height = "40px"
+    vrm2.innerText = "Ver Mais"
+    vrm2.style.width = "210px"
+    vrm2.style.height = "40px"
+    vrm3.innerText = "Ver Mais"
+    vrm3.style.width = "210px"
+    vrm3.style.height = "40px"
+
+    vrm1.style.marginTop = "10px"
+    vrm1.style.marginLeft = "105px"
+    vrm2.style.marginTop = "10px"
+    vrm2.style.marginLeft = "105px"
+    vrm3.style.marginTop = "10px"
+    vrm3.style.marginLeft = "105px"
+
+    img1.style.marginTop = "50px"
+    img2.style.marginTop = "50px"
+    img3.style.marginTop = "50px"
+
+    img1.style.marginLeft = "105px"
+    img2.style.marginLeft = "105px"
+    img3.style.marginLeft = "105px"
+
+    img1.style.boxShadow = "0px 0px 0px 2px rgb(214, 61, 228)"
+    img2.style.boxShadow = "0px 0px 0px 2px rgb(214, 61, 228)"
+    img3.style.boxShadow = "0px 0px 0px 2px rgb(214, 61, 228)"
+}
