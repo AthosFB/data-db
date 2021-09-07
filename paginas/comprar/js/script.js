e08b2fb8efc48f1db33ea7544ee432dcd13bc48d
Athos F. Bernardo <80980718+AthosFB@users.noreply.github.com>
2021-09-07 16:03:21 -0300

    voltar

---
+++ paginas/comprar/js/script.js
@@ -0,0 +1,11 @@
+let volt = window.document.getElementById("txtvolta")
+function entrou() {
+    volt.innerText = "<= Voltar"
+    volt.style.fontSize = "40px"
+    volt.style.textDecoration = "underline"
+}
+function saiu() {
+    volt.innerText = "Voltar"
+    volt.style.fontSize = "50px"
+    volt.style.textDecoration = "none"
+}
