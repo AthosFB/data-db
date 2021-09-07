b64dcec0399a41630579cdf4664100e80279f43c
Athos F. Bernardo <80980718+AthosFB@users.noreply.github.com>
2021-09-07 16:36:46 -0300

    continue

--- paginas/comprar/js/script.js
+++ paginas/comprar/js/script.js
@@ -1,11 +1,22 @@
 let volt = window.document.getElementById("txtvolta")
+let comp = window.document.getElementById("comp")
 function entrou() {
     volt.innerText = "<= Voltar"
     volt.style.fontSize = "40px"
     volt.style.textDecoration = "underline"
+    volt.style.boxShadow = "6px 6px 6px black"
 }
 function saiu() {
     volt.innerText = "Voltar"
     volt.style.fontSize = "50px"
     volt.style.textDecoration = "none"
+    volt.style.boxShadow = "3px 3px 3px black"
+}
+function entcomp() {
+    comp.style.fontSize = "65px"
+    comp.style.boxShadow = "6px 6px 6px black"
+}
+function saicomp() {
+    comp.style.fontSize = "50px"
+    comp.style.boxShadow = "3px 3px 3px black"
 }
